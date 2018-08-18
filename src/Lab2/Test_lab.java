56416ea9773723ef2c9e35cc34eba9a271a71a24
Laiman <Laiman2004@gmail.com>
2018-08-18 20:33:01 +0300

    Initial commit

---
+++ src/Lab2/Test_lab.java
@@ -0,0 +1,14 @@
+package Lab2;
+
+public class Test_lab {
+    public static void main(String[] args) {
+        int testLAB; // объявление переменной и ее тип данных (int)
+        testLAB = 7; // присвоение значению числа 7
+        int testLAB2; // объявление второго занчение
+        testLAB2 = (2+testLAB)*3;
+        System.out.println(testLAB2); // вывод на экран значение testTwo
+
+    }
+    // Задание 18.08.2018
+
+}
